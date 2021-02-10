e8de58e1347c7d6099253e5d27e57886b2615434
albertopul <76182257+albertopul@users.noreply.github.com>
2021-02-10 17:26:21 +0100

    Add strona we flasku

---
+++ strona we flasku/mypage.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect
+
+app = Flask(__name__)
+
+@app.route('/me', methods=['GET', 'POST'])
+def mypage():
+    return render_template("mypage.html")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("contact.html")
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect('/me')
